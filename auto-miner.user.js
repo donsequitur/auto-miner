1d7bbee89b229d1f656e9b8a282ac06db0645c98
Donsequitur <don.sequitur@gmail.com>
2015-07-26 03:40:20 +0000

    Fiddle with downloadURL header

--- auto-miner.user.js
+++ auto-miner.user.js
@@ -4,11 +4,11 @@
 // @version      0.4
 // @description  Place bets automatically on Salty Bet
 // @author       DonSequitur
-// @downloadURL  https://cdn.rawgit.com/donsequitur/auto-miner/master/auto-miner.user.js
+// @downloadURL  https://github.com/donsequitur/auto-miner/raw/master/auto-miner.user.js
 // @icon         https://cdn.rawgit.com/donsequitur/auto-miner/master/fat_illuminati.png
 // @match        http://www.saltybet.com/*
 // @require      https://code.jquery.com/jquery-1.11.3.js
-// @require      https://cdn.rawgit.com/dfahlander/Dexie.js/master/dist/latest/Dexie.min.js
+// @require      https://rawgit.com/dfahlander/Dexie.js/master/dist/latest/Dexie.min.js
 // @grant        none
 // ==/UserScript==
 
